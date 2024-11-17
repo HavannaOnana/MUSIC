25dced75e6abec39964c96aac6e6630a0d9d6fdf
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2024-11-17 03:09:49 +0100

    done with log in page

--- Functies/login.php
+++ Functies/login.php
@@ -33,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($fetchingUserDetails->validateUser($username, $password)) {
         session_start();
         $_SESSION['username'] = $username;
-        header('Location: /MUSIC/artist.php');
+        header('Location: /MUSIC/artist.html');
         exit;
     } else {
         echo "Invalid username or password";
@@ -47,16 +47,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LogIn Page</title>
-    <link rel="stylesheet" href="../functionstyles/login.css">
+    <link rel="stylesheet" href="../functionstyles/loging.css">
 </head>
 <body>
+    <div class="navbar">
+                <a href="../MUSIC/music.html">Home</a>
+                <a href="../MUSIC/info.html">Info</a>
+                <a href="#">ContactUs</a>
+                <a href="../MUSIC/Functies/login.php">LogIn</a>
+    </div>
+
+
     <div class="main-body">
         <h1>Login</h1>
         <form action="login.php" method="post">
             <label for="username" name="username">Username:</label><br>
-            <input type="text" name="username" required><br>
+            <input type="text" name="username" placeholder="Email" required><br>
             <label for="password" name="password">Password:</label><br>
-            <input type="password" name="password" required><br><br>
+            <input type="password" name="password" placeholder="Password" required><br><br>
+            <a href="#">Forgot Password??</a><br><br>
             <button type="submit">Submit</button>
         </form>
     </div>
