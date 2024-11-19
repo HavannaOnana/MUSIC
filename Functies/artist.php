f56910c52b0989cf80c275c17321c0d88eca177e
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2024-11-19 01:42:01 +0100

    add artist

--- Functies/artist.php
+++ Functies/artist.php
@@ -1,7 +1,35 @@
 <?php
 
+require_once "Database.php";
+
 class Artist{
   
+    private $db;
+    public function __construct(){
+        $this->db = new Database();
+    }
+
+    public function insertArtist($name,$nickname,$year,$country){
+        try{
+           $query =  "INSERT INTO artist(name,nickname,year,country) VALUES (:name,:nickname,:year,:country)";
+           $inProcess = $this->db->pdo->prepare($query);
+           $inProcess->bindParam(":name",$name);
+           $inProcess->bindParam(":nickname",$nickname);
+           $inProcess->bindParam(":year",$year);
+           $inProcess->bindParam(":country",$country);
+           $inProcess->execute();
+           echo "Artist added";
+        }
+        catch(PDOException $e){
+            if ($e->getCode() == 23000) { // SQLSTATE code for integrity constraint violation
+               echo "Duplicate song entry is not allowed!";
+          } else {
+              echo "Song not added: " . $e->getMessage();
+        }
+        }
+    }
+
+    
 
 }
 
@@ -35,6 +63,8 @@ class Artist{
             <a href="../music.html">Logout</a>
         </div>
     </div>
+
+    
     
 </body>
 </html>
